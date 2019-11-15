53beef56ddff2b3c2180dbe9da51a46820b66b62
unknown <c00220950@itcarlow.ie>
2019-11-15 12:24:22 +0000

    Implemented 1st feature

---
+++ Cannon Componant/Assets/Scripts/FireObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireObject : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform firePoint;
+    public int FireStrength;
+    public float fireDelay;
+    private float fireCooldown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireCooldown -= Time.deltaTime;
+
+        if (Input.GetKey("space") && fireCooldown <= 0)
+        {
+            fireCooldown = fireDelay;
+            GameObject bullet = Instantiate(projectile, firePoint.position, firePoint.rotation) as GameObject;
+            bullet.GetComponent<Rigidbody2D>().AddRelativeForce(transform.up * -FireStrength);
+            //bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Transform pos;
+    public int turnSpeed;
+
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.RotateAround(pos.position, Vector3.forward, turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.RotateAround(pos.position, Vector3.forward, -turnSpeed * Time.deltaTime);
+        }
+    }
+}
