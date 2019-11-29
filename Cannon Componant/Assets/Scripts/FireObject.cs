0a0f92bb642dfc8d1fa00f9533a8d2ab6f3bff19
unknown <c00220950@itcarlow.ie>
2019-11-29 15:54:32 +0000

    updated auto fire

--- Cannon Componant/Assets/Scripts/FireObject.cs
+++ Cannon Componant/Assets/Scripts/FireObject.cs
@@ -8,6 +8,9 @@ public class FireObject : MonoBehaviour
     public Transform firePoint;
     public int FireStrength;
     public float fireDelay;
+    public bool autoFire;
+    public bool fireOnce;
+    private bool hasFired = true;
     private float fireCooldown;
 
     // Update is called once per frame
@@ -15,12 +18,37 @@ public class FireObject : MonoBehaviour
     {
         fireCooldown -= Time.deltaTime;
 
-        if (Input.GetKey("space") && fireCooldown <= 0)
+        if (Input.GetKey("space") && fireCooldown <= 0 && !autoFire)
         {
             fireCooldown = fireDelay;
             GameObject bullet = Instantiate(projectile, firePoint.position, firePoint.rotation) as GameObject;
             bullet.GetComponent<Rigidbody2D>().AddRelativeForce(transform.up * -FireStrength);
             //bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10);
         }
+        
+        if (fireCooldown <= 0 && autoFire)
+        {
+            if(!fireOnce)
+            {
+                fireCooldown = fireDelay;
+                GameObject bullet = Instantiate(projectile, firePoint.position, firePoint.rotation) as GameObject;
+                bullet.GetComponent<Rigidbody2D>().AddRelativeForce(transform.up * -FireStrength);
+            }
+            else if(fireOnce && !hasFired)
+            {
+                hasFired = true;
+                fireCooldown = fireDelay;
+                GameObject bullet = Instantiate(projectile, firePoint.position, firePoint.rotation) as GameObject;
+                bullet.GetComponent<Rigidbody2D>().AddRelativeForce(transform.up * -FireStrength);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        hasFired = false;
+        Destroy(projectile);
+        projectile = other.gameObject;
+        other.transform.position = new Vector3(1000, 1000, 0);
     }
 }
